25667a10bf0a381e68f8d40d02ba577b1d1b78e6
Joginder <Joginder.kumar1991@gmail.com>
2024-04-24 10:07:51 +0530

    merge two sorted lists

--- src/main/java/com/datastrucutres/list/singleLinkedList/SinglyLinkedList.java
+++ src/main/java/com/datastrucutres/list/singleLinkedList/SinglyLinkedList.java
@@ -1,5 +1,7 @@
 package com.datastrucutres.list.singleLinkedList;
 
+import java.util.List;
+
 public class SinglyLinkedList {
     private Node head; //head of the list
 
@@ -266,6 +268,33 @@ public class SinglyLinkedList {
 
         // Set the next pointer of the node causing the loop to null
         prev.next = null;
+    }
+
+    public Node mergeTwoSortedList(Node list1, Node list2){
+
+        Node tempList = new Node(0);
+        Node current= tempList;
+
+        while(list1 != null && list2 != null){
+            if(list1.data < list2.data){
+                current.next = list1;
+                list1 = list1.next;
+            }else{
+                current.next = list2;
+                list2=list2.next;
+            }
+            current = current.next;
+
+        }
+
+        if (list1 != null) {
+            current.next = list1;
+        } else {
+            current.next = list2;
+        }
+        return tempList.next;
+
+
     }
 
 
