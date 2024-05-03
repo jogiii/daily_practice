0c780ac6c0d4be89658ecb616e982c97d3983479
Joginder <Joginder.kumar1991@gmail.com>
2024-05-03 09:41:33 +0530

    binary tree

---
+++ src/main/java/com/datastrucutres/binaryTrees/Node.java
@@ -0,0 +1,13 @@
+package com.datastrucutres.binaryTrees;
+
+public class Node {
+
+    int key;
+    Node left;
+    Node right;
+
+    Node(int item){
+        key = item;
+        left = right = null;
+    }
+}
